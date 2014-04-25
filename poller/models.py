ee21d2735598dd2ffaf4a13257f8b77e3c5890b8
akoskaaa <akos.hochrein@prezi.com>
2014-04-25 23:48:22 +0200

    updated models

--- poller/models.py
+++ poller/models.py
@@ -5,7 +5,16 @@ class Poll(models.Model):
 	question = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
+	def was_published_today(self):
+		return self.pub_date.date() == datetime.date.today()
+
+	def __unicode__(self):
+		return self.question
+
 class Choice(models.Model):
 	poll = models.ForeignKey(Poll)
 	choice = models.CharField(max_length=200)
 	votes = models.IntegerField()
+
+	def __unicode__(self):
+		return self.choice
