009db5dc5af624459cda7075a189c0fe2a7c4d9a
akoskaaa <akos.hochrein@prezi.com>
2014-04-26 23:28:00 +0200

    added an actual view

--- poller/views.py
+++ poller/views.py
@@ -1,11 +1,15 @@
 
 from django.http import HttpResponse
+from django.template import Context, loader
 from poller.models import Poll
 
 def index(request):
 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-	output = ', '.join([p.question for p in latest_poll_list])
-	return HttpResponse(output)
+	template = loader.get_template('poller/index.html')
+	context = Context({
+			'latest_poll_list': latest_poll_list,
+		})
+	return HttpResponse(template.render(context))
 
 def detail(request, poll_id):
 	return HttpResponse('You are looking at poll %s.' % poll_id)
