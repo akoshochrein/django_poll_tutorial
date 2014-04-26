dc6d702fa14ea417aea5c4a3dbb827b8883d1b09
akoskaaa <akos.hochrein@prezi.com>
2014-04-26 09:33:30 +0200

    added admin

--- poller/models.py
+++ poller/models.py
@@ -1,5 +1,6 @@
 
 from django.db import models
+import datetime
 
 class Poll(models.Model):
 	question = models.CharField(max_length=200)
@@ -7,6 +8,7 @@ class Poll(models.Model):
 
 	def was_published_today(self):
 		return self.pub_date.date() == datetime.date.today()
+	was_published_today.short_description = 'Was published today?'
 
 	def __unicode__(self):
 		return self.question
