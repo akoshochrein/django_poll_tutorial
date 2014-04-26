a8c57cee402476032148bfa529d26789d302ce15
akoskaaa <akos.hochrein@prezi.com>
2014-04-26 23:18:20 +0200

    simple view with dynamic data

--- poller/views.py
+++ poller/views.py
@@ -1,8 +1,11 @@
 
 from django.http import HttpResponse
+from poller.models import Poll
 
 def index(request):
-	return HttpResponse('Hello World, you are in the Poller Index page!')
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	output = ', '.join([p.question for p in latest_poll_list])
+	return HttpResponse(output)
 
 def detail(request, poll_id):
 	return HttpResponse('You are looking at poll %s.' % poll_id)
