eeeab6ea1e0e6665c8b877ae4c0ff3e533b3baab
akoskaaa <akos.hochrein@prezi.com>
2014-04-26 23:33:54 +0200

    404 as exception

--- poller/views.py
+++ poller/views.py
@@ -1,18 +1,18 @@
 
-from django.http import HttpResponse
-from django.template import Context, loader
+from django.http import HttpResponse, Http404
+from django.shortcuts import render_to_response
 from poller.models import Poll
 
 def index(request):
 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-	template = loader.get_template('poller/index.html')
-	context = Context({
-			'latest_poll_list': latest_poll_list,
-		})
-	return HttpResponse(template.render(context))
+	return render_to_response('poller/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-	return HttpResponse('You are looking at poll %s.' % poll_id)
+	try:
+		p = Poll.objects.get(pk=poll_id)
+	except Poll.DoesNotExist:
+		raise Http404
+	return render_to_response('poller/detail.html', {'poll': p})
 
 def results(request, poll_id):
 	return HttpResponse('You are looking at the results of poll %s.' % poll_id)
