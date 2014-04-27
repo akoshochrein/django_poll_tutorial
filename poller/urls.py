dbf0e842d38fcb4ae434156986b216e35af37d92
akoskaaa <akos.hochrein@prezi.com>
2014-04-27 22:37:59 +0200

    decoupled urls

--- poller/urls.py
+++ poller/urls.py
@@ -1,13 +1,10 @@
 
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^poller/$', 'poller.views.index'),
-	url(r'^poller/(?P<poll_id>\d+)/$', 'poller.views.detail'),
-	url(r'^poller/(?P<poll_id>\d+)/results/$', 'poller.views.results'),
-	url(r'^poller/(?P<poll_id>\d+)/vote/$', 'poller.views.vote'),
+    url(r'^poller/', include('poller_urls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- poller/views.py
+++ poller/views.py
@@ -1,6 +1,6 @@
 
 from django.http import HttpResponse, Http404
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from poller.models import Poll
 
 def index(request):
@@ -8,10 +8,7 @@ def index(request):
 	return render_to_response('poller/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-	try:
-		p = Poll.objects.get(pk=poll_id)
-	except Poll.DoesNotExist:
-		raise Http404
+	p = get_object_or_404(Poll, pk=poll_id)
 	return render_to_response('poller/detail.html', {'poll': p})
 
 def results(request, poll_id):
